d04a44e3810b9e67f1edcc6fc4e2b96de19102f9
Ikaro C. Laborda <iclaborda@msn.com>
2022-04-04 15:46:13 -0300

    feat: extracting class functionality

--- src/CourseSearch.php
+++ src/CourseSearch.php
@@ -1,25 +1,23 @@
 <?php
 
-namespace coursesearch;
+namespace ikarolaborda\coursesearch;
 
 require "./vendor/autoload.php";
 
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client();
 try {
-    $response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+    $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+    $crawler = new Crawler();
+    $searcher = new Searcher($client, $crawler);
+    $courses = $searcher->search('/cursos-online-programacao/php');
 } catch (\GuzzleHttp\Exception\GuzzleException $e) {
     $e->getMessage();
 }
 
-$html = $response->getBody();
-
-$crawler = new Crawler();
-$crawler->addHtmlContent($html);
-$courses = $crawler->filter('span.card-curso__nome');
-
-foreach ($courses as $course) {
-    file_put_contents('lista-cursos.txt', $course->textContent . PHP_EOL, FILE_APPEND);
+if(isset($courses)) {
+    foreach ($courses as $course) {
+        file_put_contents('lista-cursos.txt', $course. PHP_EOL, FILE_APPEND);
+    }
 }
---
+++ src/Searcher.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace ikarolaborda\coursesearch;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Searcher
+{
+    private ClientInterface $client;
+
+    private Crawler $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->client = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function search(string $url): array
+    {
+        $response = $this->client
+            ->request('GET',$url);
+
+        $html = (string)$response->getBody();
+
+        $this->crawler->addHtmlContent($html);
+
+        $elementCourses = $this->crawler->filter('span.card-curso__nome');
+        $courses = [];
+
+        foreach ($elementCourses as $element) {
+            $courses[] = $element->textContent;
+        }
+
+        return $courses;
+    }
+}
